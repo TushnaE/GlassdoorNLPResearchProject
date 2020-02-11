1801bf17571273f216f38955106e36604d2920cd
Tushna Eduljee <tushna@lawn-128-61-15-204.lawn.gatech.edu>
2020-02-11 12:13:13 -0500

    Completed URL search functionality

--- Glassdoor_Script.py
+++ Glassdoor_Script.py
@@ -5,9 +5,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 
 ## Bringing CSV company names into `company_list`
 company_list = []
+errored_out_companies = []
+
+company_urls = []
+
 with open('Glassdoor_Companies.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     row_num = 0
@@ -28,30 +33,37 @@ driver.find_element_by_id("userEmail").send_keys(username)
 driver.find_element_by_id ("userPassword").send_keys(password)
 driver.find_element_by_xpath ('''//*[@id="InlineLoginModule"]/div/div/div/div/div[3]/form/div[3]/div[1]/button''').click()
 
-## Testing one company in glassdoor search
-company_name = company_list[0]
-# driver.find_element_by_xpath("//*[@id='sc.keyword']").send_keys(company_name)
-            # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='sc.keyword']")))
+for x in range(6):
+    company_name = company_list[x]
 
-WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.ID, "sc.keyword"))).send_keys(company_name)
-# dropdownSelect = Select(driver.find_element_by_xpath('''//*[@id="SiteSrchTop"]/form/div/ul'''))
+    try:
+        driver.get('https://www.glassdoor.com/Reviews/index.htm')
+        driver.find_element_by_xpath('''//*[@id="KeywordSearch"]''').send_keys(company_name)
+        driver.find_element_by_xpath ('''//*[@id="HeroSearchButton"]''').click()
+        url = driver.current_url
 
-driver.find_element_by_xpath('''//*[@id="SiteSrchTop"]/form/div/ul/li[2]''').click()
+        if "Overview" in url:   ## it has gone directly to the company page
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '''//*[@id="EIProductHeaders"]/div/a[1]'''))).click()
+            
+            company_urls.append((company_name,url))
+            # print(company_name)
+            # print(url)
 
-# dropdownSelect.select_by_index(0).click()
+        else:
 
+            # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '''/html/body/div[3]/div/div/div/div[1]/div/div[1]/article/div[1]/div[2]/div[2]/a[1]'''))).click()
 
-driver.find_element_by_id("HeroSearchButton").click()
-
-
-# driver.find_element_b
-# python_button = driver.find_elements_by_id()
-
-
-
-# driver.find_element_by_id(“submit”).click()
-
-
-
+            driver.find_element_by_xpath("/html/body/div[3]/div/div/div/div[1]/div/div[1]/article/div[1]/div[2]/div[2]/a[1]").click()
+            
+            company_urls.append((company_name,url))
 
 
+            # print(company_name)
+            # print(url)
+    
+    except NoSuchElementException:
+        errored_out_companies.append(company_name)
+        continue
+
+print(company_urls)
+print(errored_out_companies)
